6c8fa9c650767505c599ded16024ac62b8a2073d
yincong <648588267@qq.com>
2018-10-15 10:51:05 +0800

    init

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"github.com/go-ole/go-ole"
+	"os"
+	"github.com/go-ole/go-ole/oleutil"
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var path string
+var name string
+
+func init() {
+	pathAddr := flag.String("p", "./", "快捷方式位置配置文件")
+	nameAddr := flag.String("n", "yc", "软件名")
+	flag.Parse()
+	path = *pathAddr
+	name = *nameAddr
+}
+
+func main() {
+	ole.CoInitializeEx(0, ole.COINIT_APARTMENTTHREADED|ole.COINIT_SPEED_OVER_MEMORY)
+	oleShellObject, err := oleutil.CreateObject("WScript.Shell")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer oleShellObject.Release()
+	wshell, err := oleShellObject.QueryInterface(ole.IID_IDispatch)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer wshell.Release()
+
+	autoStartPath := os.Getenv("appdata") + "/Microsoft/Windows/Start Menu/Programs/Startup/" + name + ".lnk"
+	cs, err := oleutil.CallMethod(wshell, "CreateShortcut", autoStartPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	idispatch := cs.ToIDispatch()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	pathLen := len(path)
+	if strings.LastIndex(path, "/") != pathLen-1 {
+		path = path + "/"
+	}
+
+	oleutil.PutProperty(idispatch, "TargetPath", path+name+".exe")
+	_, err = oleutil.CallMethod(idispatch, "Save")
+	fmt.Println("success")
+}
